cdd85eba7bf2a68a8bbd48a6733a9a56188526bd
Tayná Tinôco <a10729@alunos.ipca.pt>
2017-05-12 18:37:13 +0100

    mais atualizações

--- Bola8PROJECT/Bola8PROJECT/main.cpp
+++ Bola8PROJECT/Bola8PROJECT/main.cpp
@@ -34,13 +34,30 @@ void renderScene(void) {
 	glLoadIdentity();
 	glTranslatef(0.5f, 0, 0.5f);
 	 //glutWireSphere(1, 10, 10);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
+	//glEnable(GL_LIGHTING);
+//	glEnable(GL_LIGHT0);
 	glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
-	glDisable(GL_LIGHT0);
-	glDisable(GL_LIGHTING);
-	glFlush();
+	//glDisable(GL_LIGHT0);
+	//glDisable(GL_LIGHTING);
+	//glFlush();
+	glutSwapBuffers();
 	
+}
+
+void changeSize(int w, int h)
+{
+	float ratio;
+
+	if (h == 0) h = 1;
+	ratio = 1.0f * w / h;
+
+	glViewport(0, 0, w, h);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45, ratio, 1, 1000);
+
+	glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char **argv) {
@@ -56,10 +73,19 @@ int main(int argc, char **argv) {
 	cam.cameraSetPosition(0.0, 1.0, 5.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);
 
 	cam.cameraUpdate(0,0,0);
+
 	// Registar funções para processar eventos (callbacks)
 	glutDisplayFunc(renderScene);
+
+	//glutIdleFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	
+	//carrega o modelo
+	loadmodel();
+
 	// Entrar no ciclo de processamento do GLUT
 	glutMainLoop();
 	
+
 	return 0;
 }
