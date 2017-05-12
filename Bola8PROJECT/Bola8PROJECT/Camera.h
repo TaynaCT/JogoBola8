719dcb33fca9bb0502e227ab7f50bf11d1a04c6e
Tayná Tinôco <a10729@alunos.ipca.pt>
2017-05-12 17:42:50 +0100

    Mais coisas

---
+++ Bola8PROJECT/Bola8PROJECT/Camera.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace std {
+
+	class Camera {
+	public:
+		Camera();
+		~Camera();
+		void cameraSetPosition(double eyex, double eyey, double eyez,
+			double dirx, double diry, double dirz,
+			double upx, double upy, double upz);
+		void cameraTilt(double tilt);
+		void cameraPan(double pan);
+		void cameraMove(double move);
+		void cameraUpdate(double targetX, double targetY, double targetZ);
+
+
+	private:
+		double eyex, eyey, eyez;	// Posição da câmara (centro-de-massa)
+		double dirx, diry, dirz;	// Vector do sentido de deslocamento da câmara
+		double upx, upy, upz;		// Vector que indica a parte superior da câmara
+
+		double anglePan, angleTilt;	// Ângulos de Pan e Tilt
+		double deltaAnglePan, deltaAngleTilt;
+
+		double deltaMove;			// Move para a frente e para trás
+		double deltaSpeed;			// Aumenta ou diminui a velocidade
+		double speed;				// Constante de velocidade de deslocamento
+
+	};
+}
---
+++ Bola8PROJECT/Bola8PROJECT/main.cpp
@@ -0,0 +1,34 @@
+#include <gl/freeglut.h>
+#include <iostream>
+#include "Camera.h"
+
+using namespace std;
+
+
+void renderScene(void) {
+
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	
+	glFlush();
+	
+}
+
+int main(int argc, char **argv) {
+	Camera cam;
+
+	// Inicializar o GLUT e a janela
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowPosition(10, 10);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Programa-01");
+	// Registar funções para processar eventos (callbacks)
+	glutDisplayFunc(renderScene);
+	// Entrar no ciclo de processamento do GLUT
+	glutMainLoop();
+	
+	return 0;
+}
